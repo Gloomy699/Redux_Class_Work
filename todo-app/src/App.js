3f0aeca2b77ab00eec66b56bfc64fb16cdae285a
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-08-27 20:53:59 +0300

    add stateLess component

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import { TodoStateLess } from './components/TodoStateLess';
+function App() {
+  return (
+    <>
+       <TodoStateLess/>
+</>
+  );
+}
+
+export default App;
---
+++ todo-app/src/components/TodoStateLess.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+export const TodoStateLess = () => {
+  const [state, changeState] = useState({
+    title: 'To Do List',
+    todo: [],
+    value: ''
+  });
+
+  const handleChange = (event) => {
+    changeState({
+      ...state,
+      value: event.target.value
+    });
+  }
+
+  const handleClick = () => {
+    changeState({
+      ...state,
+      todo: state.todo.concat(state.value),
+      value: '',
+    });
+  }
+    
+
+    
+  return (
+    <>
+      <h3>{state.title}</h3>
+      <ul>
+        {
+          state.todo.map(task => <li key={task}>{task}</li>)
+        }
+          </ul>
+          
+          
+      <input type="text" onChange={handleChange} value={state.value}/>
+      <button onClick={handleClick}>add</button>
+    </>
+  );
+  
+}
+
+// function inputComponent() {
+//     return (<>
+//              <input type="text" onChange={handleChange} value={state.value}/>
+//        <button onClick={handleClick}>add</button>
+// </>
+//     )
+// }
+
+
